5f16bd2feae7abdd00b72d4c43e0234df8d22621
Grigoriy Netsman <grigoriy@netsman.org.ua>
2013-02-04 12:50:17 +0200

    added submodule for zabbix_api

--- 2.0/__init__.py
+++
@@ -1,1 +0,0 @@
-from zabbix_methods import *
--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 VERSION=$1
+DIR='zabbix-api'
 
-if [ -d zabbix-api ]; then
-	mkdir zabbix-api
+echo $VERSION
+
+while [[ ("$VERSION" != "1.8") && ("$VERSION" != "2.0") ]]; do
+	echo 'Please enter version - 1.8 or 2.0'
+	read VERSION
+done
+
+if [ ! -d $DIR ]; then
+	mkdir $DIR
 fi
+
+rm -rf $DIR/*
 
 git submodule init
 git submodule update
 
-cp $VERSION/zabbix_methods.py zabbix_api
-cp scripts/zabbix/zabbix_api.py zabbix_api
+cp $VERSION/zabbix_methods.py $DIR
+cp scripts/zabbix/zabbix_api.py $DIR
+touch $DIR/__init__.py
+
 sudo python setup.py install
 
 git update-index --assume-unchanged 1.8/zabbix_credentials.py
 git update-index --assume-unchanged 2.0/zabbix_credentials.py
+
+rm -rf zabbix-api/
